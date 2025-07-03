a8b6dcf5a1392dddad0c23301fe55c2e03770c52
Carlos Magno do Nascimento Junior <juninhonasc3@gmail.com>
2025-07-03 11:10:43 -0300

    adiciona classes iniciais do projeto

---
+++ src/test/java/com/cortaFila/cortaFila/CortaFilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cortaFila.cortaFila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CortaFilaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
